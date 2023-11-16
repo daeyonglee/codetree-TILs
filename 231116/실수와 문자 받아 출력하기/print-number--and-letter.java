f543ecb78ab015bc96e140ecfaebbebbb3883fd2
daeyonglee <dleo238@gmail.com>
2023-11-16 11:50:48 +0900

    Title: 실수와 문자 받아 출력하기, Time: 127ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 231116/실수와 문자 받아 출력하기/print-number--and-letter.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+        String c = sc.next();
+        double b = sc.nextDouble();
+        double a = sc.nextDouble();
+
+        if (Utils.checkValue(b) && Utils.checkValue(a)) {
+            System.out.println(c);
+            System.out.println(Utils.round(b));
+            System.out.println(Utils.round(a));
+        }
+
+    }
+}
+
+class Utils {
+    public static Boolean checkValue(double d) {
+        if (d >= 1 && d <= 1000) {
+            return true;
+        }
+        return false;
+    }
+
+    public static Double round(double d) {
+        return Math.round(d * 100) / 100.0;
+    }
+}
