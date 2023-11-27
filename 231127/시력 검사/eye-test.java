121c16e7c5aaa182ab5e68af936b4dd36b026648
daeyonglee <dleo238@gmail.com>
2023-11-27 19:52:03 +0900

    Title: 시력 검사, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231127/시력 검사/eye-test.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            float a = Float.parseFloat(br.readLine());
+            float b = Float.parseFloat(br.readLine());
+
+            if (a >= 1.0f && b >= 1.0f) {
+                System.out.println("High");
+            } else if (a >= 0.5f && b >= 0.5f) {
+                System.out.println("Low");
+            }
+        } catch (Exception e) {
+
+        }
+    }
+}
