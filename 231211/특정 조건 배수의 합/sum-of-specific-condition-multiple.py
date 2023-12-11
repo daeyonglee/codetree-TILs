1530ba5ea5d89ddbeea84e26f6e462c95b323164
daeyonglee <dleo238@gmail.com>
2023-12-11 11:15:29 +0900

    Title: 특정 조건 배수의 합, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 231211/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
+++ 231211/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
@@ -1,9 +1,15 @@
 a, b = input().split()
 a, b = int(a), int(b)
 
+start = a
+end = b + 1
 total = 0
-for i in range(a, b+1):
-    if i % 5 == 0:
-        total += i
+if a > b:
+    start = b
+    end = a + 1
+
+for i in range(start, end):
+        if i % 5 == 0:
+            total += i
 
 print(total)
