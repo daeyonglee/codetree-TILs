84be183d7af7b41087b4de91f0e756da9223682e
daeyonglee <dleo238@gmail.com>
2023-12-18 10:10:37 +0900

    Title: ab 사이에 없는 c, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231218/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -0,0 +1,10 @@
+a, b, c = input().split()
+a, b, c = int(a), int(b), int(c)
+
+result = False
+for i in range(a, b+1):
+    if i % c == 0:
+        result = True
+        break
+
+print("NO") if result else print("YES")
