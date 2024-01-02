01619b7195bf848b32ad60476052860c07dae739
daeyonglee <dleo238@gmail.com>
2024-01-02 18:34:52 +0900

    Title: 구구단 만들기 2, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240102/구구단 만들기 2/print-multiplication-table-2.py
+++ 240102/구구단 만들기 2/print-multiplication-table-2.py
@@ -3,7 +3,7 @@ a, b = tuple(map(int, input().split()))
 for i in range(2,9, 2):
     start = a
     for j in range(b, a-1, -1):
-        if j == 2:
+        if j == a:
             print(f"{j} * {i} = {i*j}", end=" ")
         else:
             print(f"{j} * {i} = {i*j}", end=" / ")
